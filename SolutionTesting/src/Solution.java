8c0413e97c4b74c3c631ba090dcdf1fd1a8af4ac
ismailIlMadhoune <ilmadhounismail@gmail.com>
2024-10-14 23:01:22 +0100

    UKIPC problem A

--- SolutionTesting/src/Solution.java
+++ SolutionTesting/src/Solution.java
@@ -1,71 +1,74 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+import java.util.Scanner;
 
 public class Solution {
-    public static void main(String[] args) throws Exception {
-        String[] tokens = {"2","1","+","3","*"}; // 2+1 * 3
-        Solution sol = new Solution();
-        sol.generateParenthesis(3);
-        System.out.println("----------------------------------");
-        System.out.println("OUTPUT: " + (sol.generateParenthesis(3)));
-        System.out.println("----------------------------------");
-//Input: n = 3
-//Output: ["((()))","(()())","(())()","()(())","()()()"]
-
-    }
-    
-
-    Stack<Character> stack = new Stack<>();
-    List<String> res = new ArrayList<>();
-
-    public List<String> generateParenthesis(int n) {
-        backtrack(0, 0, n);
-        return res;
-    }
-
-    private void backtrack(int openN, int closedN, int n) {
-        if (openN == closedN && closedN == n) {q
-            StringBuilder sb = new StringBuilder();
-            for (Character c: stack) {
-                sb.append(c);
-            }
-            res.add(sb.toString());
-        }
-        if (openN < n) {
-            stack.push('(');
-            backtrack(openN + 1, closedN, n);
-            stack.pop();
-        }
-        if (closedN < openN) {
-            stack.push(')');
-            backtrack(openN, closedN + 1, n);
-            stack.pop();
-        }
-    }
-    public static int[] PrefixAvg(int[] X) { // 1 , O(n^2)
-        int length = X.length - 1;
-        int[] A = new int[length];
-        for (int i = 0; i < length; i++) {
-            int s = X[0];
-            for (int j = 1; j < length; j++) {
-                if(j <= i){
-                    s += X[j];
-                }
+    public static int counter(int[][] slot,int[][] coin, int numOfSlots,int numOfCoins){
+        int count = 0;
+        for (int j = 0; j < numOfCoins; j++) {
+            for (int i = 0; i < numOfSlots; i++) {
+                if(coin[j][0] <= slot[i][0] && coin[j][1] >= slot[i][1]){
+                    count += i+1;
+                    break;
                 
+                }
             }
-            A[i] = s/(i+1);
         }
-        return A;
+        return count;
     }
-    public static int[] PrefixAvg2(int[] X){ // 2 , O(n)
-        int length = X.length - 1;
-        int[] A = new int[length];
-        int s = 0;
-        for (int i = 0; i < length; i++) {
-            s += X[i];
-            A[i] = s / (i+1);
+    public static void main(String[] args) {
+        // Initialize a Scanner to read input from standard input (console)
+        Scanner sc = new Scanner(System.in);
+        
+        // Step 1: Read the number of slots (s)
+        int s = sc.nextInt();
+
+        // Step 2: Declare an array to store slot dimensions and trigger masses
+        // Each slot will have two attributes: width (a) and trigger mass (b)
+        int[][] slots = new int[s][2];
+
+        // Step 3: Read the slots information (width and trigger mass)
+        for (int i = 0; i < s; i++) {
+            slots[i][0] = sc.nextInt(); // Slot width (a)
+            slots[i][1] = sc.nextInt(); // Slot trigger mass (b)
         }
-        return A;
-    }
+
+        // Step 4: Read the number of coins (c)
+        int c = sc.nextInt();
+
+        // Step 5: Declare an array to store coin dimensions and masses
+        // Each coin will have two attributes: thickness (u) and mass (v)
+        int[][] coins = new int[c][2];
+
+        // Step 6: Read the coins information (thickness and mass)
+        for (int j = 0; j < c; j++) {
+            coins[j][0] = sc.nextInt(); // Coin thickness (u)
+            coins[j][1] = sc.nextInt(); // Coin mass (v)
+        }
+
+        // Now you have all the input stored in 'slots' and 'coins'
+        // You can proceed to implement the logic for processing these inputs
+        
+        // Example (optional): Printing the inputs back to ensure correct reading
+        System.out.println("Slots: ");
+        for (int i = 0; i < s; i++) {
+            System.out.println("Width: " + slots[i][0] + ", Trigger Mass: " + slots[i][1]);
+        }
+
+        System.out.println("Coins:");
+        for (int j = 0; j < c; j++) {
+            System.out.println("Thickness: " + coins[j][0] + ", Mass: " + coins[j][1]);
+        }
+        System.out.println("---------------------------------");
+        System.out.println();
+        System.out.println();
+        System.out.println("OUTPUT: " + counter(slots, coins, s, c));
+        System.out.println();
+        System.out.println();        
+        System.out.println("---------------------------------");
+
+        // Close the scanner
+        sc.close();
+    
 }
+}
+
+
